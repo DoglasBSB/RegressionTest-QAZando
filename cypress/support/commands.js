3c1bc41de73bda14dd6d5baf1aac99cd03c8ee7e
DoglasBSB <doglas_bsb@hotmail.com>
2026-06-29 11:16:40 -0300

    feat: implement custom commands with session caching
    - Add guiLogin command with intercept and success assertion
    - Add sessionLogin using cy.session with cacheAcrossSpecs to avoid
      redundant logins across spec files
    - Add buscarProduto, selecionarProduto, adicionarProduto and checkout
      commands with proper intercept/wait ordering
    - Use cy.fixture for checkout form data instead of hardcoded values
    - Use cy.select for #country and #city fields (both are select elements)
    - Replace global uncaught exception suppression with targeted filter
    - Remove dead import from wd/lib/special-keys and unused finalizarCompra command
    - Add checkout.json fixture with test data

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,86 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Suppress only known third-party runtime exceptions that don't affect test behavior
+Cypress.on('uncaught:exception', (err) => {
+  const knownErrors = [
+    'ResizeObserver loop limit exceeded',
+    'Cannot read properties of undefined (reading \'removeEventListener\')',
+  ]
+  return !knownErrors.some((msg) => err.message.includes(msg))
+})
+
+Cypress.Commands.add('guiLogin', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD'),
+) => {
+  cy.intercept('GET', '**/login').as('getLogin')
+  cy.visit('/login')
+  cy.get('#user').type(username)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#btnLogin').click()
+  cy.wait('@getLogin')
+  cy.contains('h2', 'Login realizado').should('be.visible')
+})
+
+Cypress.Commands.add('sessionLogin', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD'),
+) => {
+  cy.session(username, () => cy.guiLogin(username, password), {
+    cacheAcrossSpecs: true,
+  })
+})
+
+Cypress.Commands.add('buscarProduto', (searchTerm = 'mobile') => {
+  cy.intercept('GET', '**/my-account').as('getMyAccount')
+  cy.visit('/my-account')
+  cy.wait('@getMyAccount')
+  cy.get(':nth-child(3) > .search_width > img').click()
+  cy.get('form > input').type(`${searchTerm}{enter}`)
+  cy.get('.swal2-confirm').click()
+})
+
+Cypress.Commands.add('selecionarProduto', () => {
+  cy.intercept('GET', '**/shop').as('getShop')
+  cy.visit('/shop')
+  cy.wait('@getShop')
+  cy.get('.product_wrappers_one').first().find('.title a').click()
+  cy.url().should('include', '/product-details-one/')
+})
+
+Cypress.Commands.add('adicionarProduto', () => {
+  cy.intercept('GET', '**/product-details-one/1').as('getProduct')
+  cy.visit('/product-details-one/1')
+  cy.wait('@getProduct')
+  cy.get('.links_Product_areas > .theme-btn-one').click()
+  cy.get('.modal_product_content_one > h3').should('have.text', 'Green Dress For Woman')
+  cy.get('.col-12 > .header-action-link > :nth-child(2) > .offcanvas-toggle > .fa').click()
+  cy.get('.offcanvas-cart-action-button > :nth-child(1) > .theme-btn-one').click()
+  cy.get(':nth-child(4) > .product_name > a').should('be.visible')
+})
+
+Cypress.Commands.add('checkout', () => {
+  cy.fixture('checkout').then((data) => {
+    cy.intercept('GET', '**/checkout-one').as('getCheckout')
+    cy.visit('/checkout-one')
+    cy.wait('@getCheckout')
+
+    cy.get('#fname').type(data.firstName)
+    cy.get('#lname').type(data.lastName)
+    cy.get('#cname').type(data.companyName)
+    cy.get('#email').type(data.email)
+    cy.get('#country').select(data.country)
+    cy.get('#city').select(data.city)
+    cy.get('#zip').type(data.zipCode)
+    cy.get('#faddress').type(data.address)
+    cy.get('#messages').type(data.message)
+
+    cy.get('.form-check-label').click()
+    cy.get('.checkout-area-bg > .theme-btn-one').click()
+
+    cy.get(':nth-child(2) > h3').should('have.text', 'Billings Information registred with success!')
+    cy.get(':nth-child(2) > .check-heading > h3').should('be.visible')
+    cy.get(':nth-child(2) > :nth-child(2) > .theme-btn-one').click()
+
+    cy.contains('h2', 'Order success!').should('be.visible')
+    cy.get('.offer_modal_left > h3').should('have.text', 'Congrats! Your order was created with sucess!')
+  })
+})
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '@walmyr-filho/cy-press'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
